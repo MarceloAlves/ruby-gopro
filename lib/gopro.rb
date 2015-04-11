6fd5e6af4e995c4404800ea0becad0a5b0ae3a0f
Marcelo Alves <selfmadecelo@gmail.com>
2015-04-10 23:29:38 -0700

    Added a few modes

--- lib/gopro.rb
+++ lib/gopro.rb
@@ -1,4 +1,6 @@
 require "gopro/version"
+require 'net/http'
+require 'uri'
 
 module GoPro
   class Hero
@@ -6,7 +8,26 @@ module GoPro
 
     def initialize(options={})
       @gopro_ip = options[:ip] || '10.5.5.9'
-      @gopro_password = options[:password]
+    end
+
+    def camera_mode
+      Net::HTTP.get(@gopro_ip, '/gp/gpControl/command/mode?p=0')
+    end
+
+    def photo_mode
+      Net::HTTP.get(@gopro_ip, '/gp/gpControl/command/mode?p=1')
+    end
+
+    def burst
+      Net::HTTP.get(@gopro_ip, '/gp/gpControl/command/sub_mode?mode=2&sub_mode=0')
+    end
+
+    def timelapse
+      Net::HTTP.get(@gopro_ip, '/gp/gpControl/command/sub_mode?mode=2&sub_mode=1')
+    end
+
+    def nightlapse
+      Net::HTTP.get(@gopro_ip, '/gp/gpControl/command/sub_mode?mode=2&sub_mode=2')
     end
   end
 end
--- lib/gopro/version.rb
+++ lib/gopro/version.rb
@@ -1,3 +1,3 @@
 module GoPro
-  VERSION = "0.0.1"
+  VERSION = "0.0.1pre"
 end
